315d630c9d2252f7dd3e8ea528c296771eac3db9
Julio Zittei <julio.zittei@hotmail.com>
2016-11-22 22:48:12 -0200

    Implementação e mapeamento da classe EnderecoUsuario

---
+++ src/main/java/br/com/opeads/model/EnderecoUsuario.java
@@ -0,0 +1,153 @@
+package br.com.opeads.model;
+
+import java.io.Serializable;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "endereco_usuario")
+public class EnderecoUsuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7666097250216026835L;
+	
+	@Id
+	@OneToOne(cascade = {CascadeType.REMOVE})
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+
+	private Integer numero;
+	
+	private String logradouro;
+	
+	private String bairro;
+	
+	private String cidade;
+	
+	private String estado;
+	
+	private String cep;
+	
+	private String complemento;
+	
+	private String tipo;
+	
+	@Column(columnDefinition = "bit default 0")
+	private Boolean principal;
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Boolean getPrincipal() {
+		return principal;
+	}
+
+	public void setPrincipal(Boolean principal) {
+		this.principal = principal;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		EnderecoUsuario other = (EnderecoUsuario) obj;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
+			return false;
+		return true;
+	}
+	
+	
+
+}
--- src/main/java/br/com/opeads/model/Funcionario.java
+++
@@ -1,5 +0,0 @@
-package br.com.opeads.model;
-
-public class Funcionario {
-
-}
